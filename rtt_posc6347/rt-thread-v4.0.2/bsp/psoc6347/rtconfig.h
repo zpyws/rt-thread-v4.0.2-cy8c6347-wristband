d6f19c9578ae319931fcc41302e49b93b213b318
yangwensen <yangws@landicorp.com>
2020-03-13 17:36:52 +0800

    menuconfig增加qspi配置项

--- rtt_posc6347/rt-thread-v4.0.2/bsp/psoc6347/rtconfig.h
+++ rtt_posc6347/rt-thread-v4.0.2/bsp/psoc6347/rtconfig.h
@@ -80,6 +80,8 @@
 #define RT_PIPE_BUFSZ 512
 #define RT_USING_SERIAL
 #define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_SPI
+#define RT_USING_QSPI
 
 /* Using USB */
 
@@ -160,6 +162,7 @@
 
 #define BSP_USING_UART
 #define BSP_USING_UART6
+#define BSP_USING_QSPI
 
 /* Board extended module Drivers */
 
