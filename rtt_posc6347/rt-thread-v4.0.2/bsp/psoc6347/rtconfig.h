a22258858148f5871f57322c177a740a99caa750
yangwensen <yangws@landicorp.com>
2020-04-08 17:11:03 +0800

    [usbd]enable rt-thread usb device & cdc virtual com component [info]code=177212 sram=12272

--- rtt_posc6347/rt-thread-v4.0.2/bsp/psoc6347/rtconfig.h
+++ rtt_posc6347/rt-thread-v4.0.2/bsp/psoc6347/rtconfig.h
@@ -102,6 +102,16 @@
 
 /* Using USB */
 
+#define RT_USING_USB_DEVICE
+#define RT_USBD_THREAD_STACK_SZ 4096
+#define USB_VENDOR_ID 0x0FFE
+#define USB_PRODUCT_ID 0x0001
+#define _RT_USB_DEVICE_CDC
+#define RT_USB_DEVICE_CDC
+#define RT_VCOM_TASK_STK_SIZE 512
+#define RT_VCOM_SERNO "32021919830108"
+#define RT_VCOM_SER_LEN 14
+#define RT_VCOM_TX_TIMEOUT 1000
 
 /* POSIX layer and C standard library */
 
@@ -222,6 +232,7 @@
 #define BSP_USING_ADC1
 #define BSP_USING_ONCHIP_RTC
 #define BSP_RTC_USING_LSE
+#define BSP_USING_USBD
 
 /* Board extended module Drivers */
 
