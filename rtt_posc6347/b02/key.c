1fc1222e6bb1721fa76fb520ddb6c727daccfea6
yangwensen <fjzpyws@qq.com>
2020-07-22 15:42:04 +0800

    home key interrupt routine

--- rtt_posc6347/b02/key.c
+++ rtt_posc6347/b02/key.c
@@ -9,10 +9,13 @@ static void home_key_isr(void *args)
     rt_kprintf("port7 int\n");
 }
 //************************************************************************************************************
-void home_key_init(void)
+static int home_key_init(void)
 {
     rt_pin_mode(HOME_KEY_PIN, PIN_MODE_INPUT);
     rt_pin_attach_irq(HOME_KEY_PIN, PIN_IRQ_MODE_RISING, home_key_isr, RT_NULL);
     rt_pin_irq_enable(HOME_KEY_PIN, PIN_IRQ_ENABLE);
+
+    return RT_EOK;
 }
+INIT_BOARD_EXPORT(home_key_init);
 //************************************************************************************************************
